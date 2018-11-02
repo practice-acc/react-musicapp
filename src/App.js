ae843e428ec094fd41ccb3ce89904d5386d2f2cf
Jon White <jonwhite@Jons-MacBook-Pro.local>
2018-11-01 22:40:09 -0400

    created Navbar using bootstrap. Now were are setting up the layout for each song track, with its own components.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
 import './App.css';
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Hello!</h1>
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index}/>
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     );
-  }
-}
-
+  };
+};
 export default App;
